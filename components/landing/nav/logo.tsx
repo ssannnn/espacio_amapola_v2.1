4a2228b99cc7fe311487345b3459c0761b1e0f60
Santi Gonzalez <santi.gnzlz@proton.me>
2025-05-01 15:25:29 -0300

    Fix logo sizing in nav menu

--- components/landing/nav/logo.tsx
+++ components/landing/nav/logo.tsx
@@ -7,9 +7,9 @@ import Image from "next/image"
 export default function Logo() {
   return (
     <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-      <Link href="/" scroll={false} className="flex items-center gap-2 text-2xl text-orange-500">
-        <Image src="/logo_s.png" alt="logo" width={24} height={24} className="h-[1.2em] w-[1.2em]" />
-        <span className="hidden md:block font-display font-light tracking-wide">Espacio Amapola</span>
+      <Link href="/" scroll={true} className="flex items-center gap-2">
+        <Image src="/logo_s.png" alt="logo" width={24} height={24} className="h-[1.6em] w-[1.6em]" aria-hidden="true" />
+        <span className="hidden md:block font-display font-light tracking-wide text-xl text-orange-500">Espacio Amapola</span>
       </Link>
     </motion.div>
   )
