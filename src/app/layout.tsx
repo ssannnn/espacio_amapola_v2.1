30d5d5974aa79d24e4aeefe9e5500918dde8a845
Santi Gonzalez <santi.gnzlz@proton.me>
2025-05-05 11:41:40 -0300

    Add google fonts via nextjs imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type React from "react";
 import "@/src/styles/globals.css";
-import { ThemeProvider } from "@/src/components/theme-provider";
+import { ThemeProvider } from "@/src/components/providers/theme-provider";
+import { inter, sourGummy } from "@/src/fonts/fonts";
 
 export const metadata = {
   title: "Espacio Amapola",
   description: "Abrazá tu maternidad",
-  // Add the Google Fonts links to the metadata
   // metadataBase: new URL(""),
   openGraph: {
     title: "Espacio Amapola",
@@ -21,20 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="font-sans">
+    <html lang="es" className={`${inter.variable} ${sourGummy.variable}`}>
+      <body className="font-sans">     
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
